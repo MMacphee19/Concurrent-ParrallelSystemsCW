a26466f0eecd49b5871c4a5d5f76967165cabb4f
MagnusMacphee <123734481+Gustafsonn@users.noreply.github.com>
2025-11-15 13:32:54 +0000

    changed image loacation

--- cw1/main.cpp
+++ cw1/main.cpp
@@ -97,7 +97,7 @@ int main()
     std::srand(static_cast<unsigned int>(std::time(NULL)));
 
     // example folder to load images
-    const char* image_folder = "images/unsorted";
+    const char* image_folder = "../images/unsorted";
     if (!fs::is_directory(image_folder))
     {
         printf("Directory \"%s\" not found: please make sure it exists, and if it's a relative path, it's under your WORKING directory\n", image_folder);
